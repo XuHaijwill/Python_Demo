8284d3ea52518035bbf19d0894fdd4aa9792593f
XuHaijwill <zuiwoxuanlou@163.com>
2025-04-23 22:48:12 +0800

    args demo

---
+++ src/base/arg_demo.py
@@ -0,0 +1,45 @@
+def foo(param1, *param2):
+    """
+    单星号（*）：*agrs
+    将所以参数以元组(tuple)的形式导入：
+    """
+    print(param1)
+    print(param2)
+
+
+foo(1, 2, 3, 4, 5)
+
+print("------------------------------------------------------------")
+
+def bar(param1, **param2):
+    """
+    双星号（**）：**kwargs
+    将参数以字典的形式导入
+    """
+    print(param1)
+    print(param2)
+
+
+bar(1, a=2, b=3)
+
+print("------------------------------------------------------------")
+
+def foo1(bar, lee):
+    print(bar, lee)
+
+
+l = [1, 2]
+
+# 单星号的另一个用法是解压参数列表：
+foo1(*l)
+
+print("------------------------------------------------------------")
+
+def foo2(a, b=10, *args, **kwargs):
+    print(a)
+    print(b)
+    print(args)
+    print(kwargs)
+
+
+foo2(1, 2, 3, 4, e=5, f=6, g=7)
